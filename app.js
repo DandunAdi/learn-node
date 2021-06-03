970bce0fc42b1d74f75e1cb58b6c0d3f86eacf35
Dandun Adi <dandun.adi@gmail.com>
2021-06-03 15:19:43 +0700

    ejs

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ const express = require("express");
 
 const app = express();
 app.listen(3000);
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  // res.send("<h1>Hello</h1>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create Blog" });
 });
 
 // Redirects
@@ -19,5 +37,5 @@ app.get("/about-us", (req, res) => {
 
 // 404
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
