b6422a2d5e6c9c0b8ea206153a455390ff6da4be
Dandun Adi <dandun.adi@gmail.com>
2021-06-03 15:43:27 +0700

    middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 app.listen(3000);
 app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const blogs = [
